6601baecc6947b4ca80a7b1ac68eb4b2193f727d
Mykola Nakonechnuy <teddykolia@ukr.net>
2026-03-03 21:32:43 +0200

    add models User, Author, Tag, Book; AUTH_USER_MODEL contant to settings; database to gitignore

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,38 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+from booksharing.settings import AUTH_USER_MODEL
+
+
+class User(AbstractUser):
+    telegram_tag = models.CharField(max_length=255)
+
+    def __str__(self):
+        return (f"{self.first_name} {self.last_name} ({self.username}). "
+                f"Telegram: @{self.telegram_tag}")
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.ManyToManyField(Author, related_name="books")
+    tag = models.ManyToManyField(Tag, related_name="books")
+    owner = models.ForeignKey(
+        AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="owned_books",
+    )
+    borrower = models.ForeignKey(
+        AUTH_USER_MODEL,
+        null=True, blank=True,
+        on_delete=models.SET_NULL,
+        related_name="borrowed_books",
+    )
