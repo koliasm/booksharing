edb71f2aa2d8645d90a36fd53973245a51459ad5
Mykola Nakonechnuy <teddykolia@ukr.net>
2026-03-04 13:39:27 +0200

    add tests for str method in catalog models

---
+++ catalog/tests/test_models.py
@@ -0,0 +1,62 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from catalog.models import Author, Tag, Book
+
+class ModelTest(TestCase):
+    def test_user_str(self):
+        user = get_user_model().objects.create_user(
+            username="username",
+            password="password",
+            first_name="Test_first",
+            last_name="Test_last",
+            telegram_tag="test_tag",
+        )
+        self.assertEqual(
+            str(user),
+            f"{user.first_name} {user.last_name} ({user.username}). "
+            f"Telegram: @{user.telegram_tag}")
+
+    def test_author_str(self):
+        author = Author.objects.create(
+            first_name="Test_first",
+            last_name="Test_last",
+        )
+        self.assertEqual(
+            str(author),
+            f"{author.first_name} {author.last_name}"
+        )
+
+    def test_book_str(self):
+        owner = get_user_model().objects.create_user(
+            username="owner",
+            password="password1",
+            first_name="Owner_first",
+            last_name="Owner_last",
+            telegram_tag="Owner_tag",
+        )
+        borrower = get_user_model().objects.create_user(
+            username="borrower",
+            password="password2",
+            first_name="Borrower_first",
+            last_name="Borrower_last",
+            telegram_tag="Borrower_tag",
+        )
+        author = Author.objects.create(
+            first_name="Author_first",
+            last_name="Author_last",
+        )
+        tag = Tag.objects.create(
+            name="fiction",
+        )
+        book = Book.objects.create(
+            title="Harry Potter",
+            owner=owner,
+            borrower=borrower,
+        )
+        book.author.add(author)
+        book.tag.add(tag)
+
+        self.assertEqual(
+            str(book),
+            f"{book.title} - {book.author}"
+        )
