165c1d9364cc1c7aae471398cecfec8592fa9d9f
Mykola Nakonechnuy <teddykolia@ukr.net>
2026-03-12 21:33:28 +0200

    add catalog urls, home view, and base/index templates

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
 
-# Create your views here.
+from .models import Book, Author, Tag
+from django.contrib.auth import get_user_model
+
+
+def index(request: HttpRequest) -> HttpResponse:
+    num_books = Book.objects.count()
+    num_authors = Author.objects.count()
+    num_tags = Tag.objects.count()
+    num_users = get_user_model().objects.count()
+
+    context = {
+        "num_books": num_books,
+        "num_authors": num_authors,
+        "num_tags": num_tags,
+        "num_users": num_users,
+    }
+    return render(request, "catalog\index.html", context=context)
